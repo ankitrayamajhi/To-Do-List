bfdaeccab8eff8440fa936c1df9a3c0e266ee420
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2022-10-19 16:02:11 +0545

    Deployed by Ankit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  const [userInput, setUserInput] = useState()
+  const [todos, setTodos] = useState([])
+  const addData = () =>{
+   setTodos([...todos,userInput])
+  }
+  return <div>
+     <h1> Todo Lists</h1>
+     <input onChange={(event)=> setUserInput(event.target.value)} type={"text"}></input>
+     <button onClick={() => addData()} >Add</button>
+     <ul>
+      {
+        todos.map((todo,index)=> <li key={index}>{todo}</li>)
+      }
+     </ul>
+  </div>
 }
-
-export default App;
+export default App
